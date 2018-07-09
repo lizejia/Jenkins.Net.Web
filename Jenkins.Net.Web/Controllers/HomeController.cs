3a392a20235a5bf6f176c5aafeab7b73bd12507c
泽嘉 李 <romanzli@outlook.com>
2018-07-09 11:37:12 +0800

    更新显示版本

--- Jenkins.Net.Web/Controllers/HomeController.cs
+++ Jenkins.Net.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,8 +9,10 @@ namespace Jenkins.Net.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string PublishVersion = ConfigurationManager.AppSettings["PublishVersion"];
         public ActionResult Index()
         {
+            ViewBag.PublishVersion = PublishVersion;
             return View();
         }
 
